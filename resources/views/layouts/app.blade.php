885f4721b6ce5a7914d4479db1e16dfdebac7feb
Gabriel Cabral <gabrielcabral@healthhunters.com.br>
2020-11-18 07:17:52 -0300

    Iniciando CRUD de usuários. Listagem e Exclusão

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -55,6 +55,7 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a href="{{route('user.index')}}" class="dropdown-item">Gestão de Usuários</a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
